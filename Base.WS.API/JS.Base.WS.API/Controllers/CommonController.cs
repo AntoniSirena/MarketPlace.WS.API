9458197dcbbf20cd9d506865b07bc092a7874e1c
AntoniSirena <lic-juansirena@hotmail.com>
2019-12-28 18:25:29 -0400

    Creation CommonController

---
+++ Base.WS.API/JS.Base.WS.API/Controllers/CommonController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JS.Base.WS.API.Controllers
+{
+    [RoutePrefix("api/common")]
+    public class CommonController : ApiController
+    {
+
+    }
+}
