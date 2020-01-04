7a9172136cdd9ad1d229c6cefc9603e5fe090fb1
AntoniSirena <lic-juansirena@hotmail.com>
2020-01-04 17:54:58 -0400

    new changes

--- Base.WS.API/JS.Base.WS.API/Controllers/CommonController.cs
+++ Base.WS.API/JS.Base.WS.API/Controllers/CommonController.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using JS.Base.WS.API.DBContext;
+using JS.Base.WS.API.Models.PersonProfile;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace JS.Base.WS.API.Controllers
@@ -10,6 +9,29 @@ namespace JS.Base.WS.API.Controllers
     [RoutePrefix("api/common")]
     public class CommonController : ApiController
     {
+        MyDBcontext db = new MyDBcontext();
 
+        [HttpGet]
+        [Route("getGenders")]
+        public List<GenderResponse> getGenders()
+        {
+            List<GenderResponse> genders = db.Genders.Where(x => x.IsActive == true).Select(y => new GenderResponse
+            {
+                Id = y.Id,
+                Description = y.Description,
+                ShortName = y.ShortName
+            }).ToList();
+
+            return genders;
+        }
+
+
+
+        public class GenderResponse
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public string ShortName { get; set; }
+        }
     }
 }
