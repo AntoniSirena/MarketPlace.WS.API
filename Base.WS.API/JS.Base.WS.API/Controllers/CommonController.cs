fb03dcadd05e99580a0729d15747c73bf2477c58
AntoniSirena <lic-juansirena@hotmail.com>
2020-02-22 08:24:52 -0400

    Creation endPoint P-6U22R4I MINGW64 /c/Git/JS.Base.WS.API (Develop)

--- Base.WS.API/JS.Base.WS.API/Controllers/CommonController.cs
+++ Base.WS.API/JS.Base.WS.API/Controllers/CommonController.cs
@@ -1,5 +1,7 @@
 ﻿using JS.Base.WS.API.DBContext;
 using JS.Base.WS.API.DTO.Common;
+using JS.Base.WS.API.Helpers;
+using JS.Base.WS.API.Models.Authorization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -10,10 +12,11 @@ namespace JS.Base.WS.API.Controllers
     public class CommonController : ApiController
     {
         MyDBcontext db = new MyDBcontext();
+        long currenntUserId = CurrentUser.GetId();
 
         [HttpGet]
-        [Route("getGenders")]
-        public List<GenderDto> getGenders()
+        [Route("GetGenders")]
+        public List<GenderDto> GetGenders()
         {
             List<GenderDto> genders = db.Genders.Where(x => x.IsActive == true).Select(y => new GenderDto
             {
@@ -25,6 +28,22 @@ namespace JS.Base.WS.API.Controllers
             return genders;
         }
 
+        [HttpGet]
+        [Route("GetInfoCurrentUser")]
+        public IHttpActionResult GetInfoCurrentUser()
+        {
+            var result = db.Users.Where(x => x.Id == currenntUserId).Select(x => new
+            {
+                UserName = x.UserName,
+                Password = x.Password,
+                Name = x.Name,
+                Surname = x.Surname,
+                EmailAddress = x.EmailAddress,
+                Image = x.Image
+            }).FirstOrDefault();
+
+            return Ok(result);
+        }
 
     }
 }
